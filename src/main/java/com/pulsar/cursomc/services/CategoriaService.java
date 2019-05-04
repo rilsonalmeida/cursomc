fa82cf1bfbade052eda410945172c3abfc1e2861
rilsonalmeida <rilson.almeida@gmail.com>
2019-05-04 20:13:59 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/pulsar/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.pulsar.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pulsar.cursomc.domain.Categoria;
+import com.pulsar.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
